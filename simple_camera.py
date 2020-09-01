8dea5998008763993d3ef0ab0663a83ec7119d05
Mark Citron <mark_citron@hotmail.com>
2020-09-01 16:21:05 -0700

    Added simple camera script

---
+++ simple_camera.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import cv2
+
+def main():
+    cam = cv2.VideoCapture(0)
+    cv2.namedWindow("Simple Camera")
+
+    img_counter = 0
+
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            print("Failed to grab frame")
+            breakcv2.imshow("Simple Camera", frame)
+
+            k = cv2.waitKey(1)
+            if k%256 == 27:
+                print("Escape hit, see you later.")
+                break
+            if k%256 == 32:
+                img.name = "frame_capture_{}.png".format(img_counter)
+                cv2.imwrite(img_name, frame)
+                print("{} written".format(img_name))
+                img_counter += 1
+    
+    cam.release()
+    cv2.detroyAllWindows()
+
+if __name__ == "__main__":
+    main()
